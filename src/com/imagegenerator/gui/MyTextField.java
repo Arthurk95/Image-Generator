2ac271913b2c4f9c289c6a1d0248e89528be9935
Arthur Kharit <arthurkharit@csus.edu>
2019-05-23 19:48:53 -0700

    Textfield is now a rounded-rectangle

--- src/com/imagegenerator/gui/MyTextField.java
+++ src/com/imagegenerator/gui/MyTextField.java
@@ -8,25 +8,64 @@ public class MyTextField extends JTextField {
     public static final Color BG_COLOR = new Color(58,58,58);
     public static final Color FG_COLOR = new Color(207,207,207);
     public static final Color DISABLED_BG_COLOR = new Color(32,32,32);
-    public static final Color DISABLED_FG_COLOR = new Color(50,50,50);
+    public static final Color DISABLED_FG_COLOR = new Color(70,70,70);
 
+    private Color currentFGColor = FG_COLOR;
+    private Color currentColor = BG_COLOR;
+    private Color currentBorderColor = BORDER_COLOR;
     public MyTextField(){
+        super();
+        setOpaque(false);
         format();
     }
 
     public void format(){
-        this.setBackground(BG_COLOR);
-        this.setForeground(FG_COLOR);
+        this.setForeground(currentFGColor);
+        this.setDisabledTextColor(DISABLED_FG_COLOR);
         normalBorder();
         Font font = new Font("Courier New", Font.PLAIN,12);
         this.setFont(font);
     }
 
+    protected void paintComponent(Graphics g){
+        //g.clearRect(0,0,this.getWidth(),this.getHeight());
+        this.setForeground(currentFGColor);
+
+        g.setColor(currentColor);
+        g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 10, 10);
+        super.paintComponent(g);
+    }
+
+    protected void paintBorder(Graphics g){
+        g.setColor(currentBorderColor);
+        g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 10, 10);
+    }
+
     public void redBorder(){
-        this.setBorder(BorderFactory.createLineBorder(Color.RED));
+        if (this.isEnabled())
+            currentBorderColor = Color.RED;
+
+    }
+
+    public void setEnabled(boolean enabled){
+        super.setEnabled(enabled);
+        if(enabled)
+            setEnabled();
+        else setDisabled();
+    }
+
+    public void setEnabled(){
+        currentColor = BG_COLOR;
+        currentFGColor = FG_COLOR;
+    }
+
+    public void setDisabled(){
+        currentColor = DISABLED_BG_COLOR;
+        currentFGColor = DISABLED_FG_COLOR;
     }
 
     public void normalBorder(){
-        this.setBorder(BorderFactory.createLineBorder(BORDER_COLOR));
+        currentBorderColor = BORDER_COLOR;
+        //this.setBorder(BorderFactory.createLineBorder(BORDER_COLOR));
     }
 }
