cdeaf66d751ebaffce7884b6242cec787b63c58f
Arthur Kharit <arthurkharit@csus.edu>
2019-05-25 13:39:18 -0700

    Custom JLabel for field labels

---
+++ src/com/imagegenerator/gui/MyFieldLabel.java
@@ -0,0 +1,12 @@
+package com.imagegenerator.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFieldLabel extends JLabel {
+    public MyFieldLabel(){
+        this.setFont(new Font("Calibri", Font.PLAIN,11));
+        this.setOpaque(false);
+        this.setForeground(new Color(212,212,212));
+    }
+}
