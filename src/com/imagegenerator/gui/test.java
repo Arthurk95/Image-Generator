272afe3d03a6a886b801ee549dbd272c033fae0f
Arthur Kharit <arthurkharit@csus.edu>
2019-05-25 13:39:58 -0700

    form ideas

--- src/com/imagegenerator/gui/test.java
+++ src/com/imagegenerator/gui/test.java
@@ -3,5 +3,28 @@ package com.imagegenerator.gui;
 import javax.swing.*;
 
 public class test {
+    private JPanel mainPanel;
+    private JPanel dimensionsPanel;
+    private MyTextFieldLine imageWidthTF;
+    private MyTextFieldLine imageHeightTF;
+    private MyTextFieldLine topGradientTF;
+    private MyTextFieldLine botGradientTF;
+    private JPanel colorsPanel;
+    private MyTextField mainColorTF;
+    private MyTextField gradientColorTF;
+    private JPanel textPanel;
+    private MyTextField textFontSizeTF;
+    private JPanel iteratePanel;
+    private MyTextField iterationsTF;
     private JCheckBox iterativeCheckBox;
+    private MyTextField textContentTF;
+    private MyTextField textFontTF;
+    private MyTextField textColorTF;
+    private JCheckBox textCheckBox;
+    private JTextField directoryTF;
+    private JButton generateImageFileButton;
+    private JComboBox extensionDropDown;
+    private MyTextField fileNameTF;
+    private PreviewPanel imagePreviewPanel;
+    private JButton generatePreviewButton;
 }
