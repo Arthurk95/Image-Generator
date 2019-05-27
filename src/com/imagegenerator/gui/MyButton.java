fc189e5f48c4ea152d97c2e4eade21fa6c7690a7
Arthur Kharit <arthurkharit@csus.edu>
2019-05-26 21:59:37 -0700

    overridden paintComponent to draw rounded rect and draw centered text

--- src/com/imagegenerator/gui/MyButton.java
+++ src/com/imagegenerator/gui/MyButton.java
@@ -12,8 +12,8 @@ public class MyButton extends JButton {
     private final Font font = new Font("Calibri", Font.PLAIN, 11);
 
     public MyButton(){
-        this.setBackground(BG_COLOR);
         this.setForeground(FG_COLOR);
+        this.setOpaque(false);
         this.setFont(font);
         this.setMargin(new Insets(4,5,4,5));
         this.addMouseListener(new MouseAdapter() {
@@ -28,10 +28,35 @@ public class MyButton extends JButton {
         });
     }
 
-    protected void paintBorder(Graphics g) {
-        g.setColor(BORDER_COLOR);
-        g.drawRoundRect(0, 0, this.getWidth()-1, this.getHeight()-1, 10, 10);
+    protected void paintComponent(Graphics g){
+        Graphics2D graph2D = (Graphics2D)g;
+        graph2D.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        graph2D.setRenderingHint(
+                RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graph2D.setColor(BG_COLOR);
+        graph2D.fillRoundRect(0, 0, this.getWidth()-1, this.getHeight()-1, 10, 10);
+        graph2D.setColor(BORDER_COLOR);
+        graph2D.drawRoundRect(0, 0, this.getWidth()-1, this.getHeight()-1, 10, 10);
+        centerString(graph2D);
     }
 
+    protected void paintBorder(Graphics g) {
+
+    }
+
+    private void centerString(Graphics2D g){
+        FontMetrics metrics = g.getFontMetrics(font);
+        // Determine the X coordinate for the text
+        int x = (this.getWidth() - metrics.stringWidth(this.getText()))/2;
+        // Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
+        int y = ((this.getHeight() - metrics.getHeight())/2) + metrics.getAscent();
+        g.setFont(font);
+        g.setColor(FG_COLOR);
+        g.drawString(this.getText(), x, y);
+    }
 
 }
