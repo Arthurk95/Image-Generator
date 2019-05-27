49037e680dd8a2543f1cd0b9b7992c4e3bc39818
Arthur Kharit <arthurkharit@csus.edu>
2019-05-26 22:00:34 -0700

    changed colors to look more natural

--- src/com/imagegenerator/gui/MyRadioButton.java
+++ src/com/imagegenerator/gui/MyRadioButton.java
@@ -6,22 +6,22 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyRadioButton extends JRadioButton {
-    private int width = 50;
-    private int height = 25;
+    private int width = 40;
+    private int height = 20;
 
     public MyRadioButton(){
         this.setForeground(Utility.RB_MAIN_COLOR);
         this.setOpaque(false);
         this.setFont(Utility.MAIN_FONT);
-        this.setMinimumSize(new Dimension(width+5, height+5));
+        this.setMinimumSize(new Dimension(width+20, height+10));
     }
 
     public void paint(Graphics g){
         Graphics2D g2 = (Graphics2D)g;
         Color red = new Color(150,80,80);
         Color darkerRed = new Color(140,70,70);
-        Color blue = Utility.TF_SELECTED_BORDER_COLOR;
-        Color darkerBlue = new Color(40,70,110);
+        Color green = new Color(50,150,70);
+        Color darkerGreen = new Color(40,140,60);
 
         g2.setRenderingHint(
                 RenderingHints.KEY_ANTIALIASING,
@@ -30,25 +30,29 @@ public class MyRadioButton extends JRadioButton {
         GradientPaint gradientRed = new GradientPaint(
                 0, 0, darkerRed, 0, height, red);
         GradientPaint gradientBlue = new GradientPaint(
-                0, 0, darkerBlue, 0, height, blue);
+                0, 0, darkerGreen, 0, height, green);
         GradientPaint circleGradient = new GradientPaint(
                 0, 0, Color.LIGHT_GRAY, 0, height, Color.GRAY);
 
         if(this.isSelected()){
             g2.setPaint(gradientBlue);
             g2.fillRoundRect(1,1, width, height, height,height);
-            g2.setColor(Color.DARK_GRAY);
+            g2.setColor(Color.darkGray);
             g2.drawRoundRect(0,0, width+1, height+1, height,height);
             g2.setPaint(circleGradient);
             g2.fillOval(width-(height)+1, 1, height,height);
+            g2.setColor(Color.DARK_GRAY);
+            g2.drawOval(width-(height)+1, 1, height,height);
         }
         else {
             g2.setPaint(gradientRed);
             g2.fillRoundRect(1,1, width, height, height,height);
-            g2.setColor(Color.DARK_GRAY);
+            g2.setColor(Color.darkGray);
             g2.drawRoundRect(0,0, width+1, height+1, height,height);
             g2.setPaint(circleGradient);
-            g2.fillOval(0, 1, height,height);
+            g2.fillOval(1, 1, height,height);
+            g2.setColor(Color.DARK_GRAY);
+            g2.drawOval(1, 1, height,height);
         }
 
     }
