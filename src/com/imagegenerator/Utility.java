c450671ae9a87748437e0bbf91067010f01508ac
Arthur Kharit <arthurkharit@csus.edu>
2019-05-29 23:40:51 -0700

    Circle checkbox. fills circle when selected

--- src/com/imagegenerator/Utility.java
+++ src/com/imagegenerator/Utility.java
@@ -6,11 +6,13 @@ public class Utility {
     public final static Color MAIN_BG_COLOR = new Color(69,77,81);
     public final static Color BORDER_COLOR = new Color(100,100,100);
     public final static Color TF_BG_COLOR = new Color(58,58,58);
-    public final static Color TF_FG_COLOR = new Color(207,207,207);
+    public final static Color TF_FONT_COLOR = new Color(207,207,207);
+    public final static Color TF_HINT_COLOR = new Color(150,150,150);
     public final static Color TF_DISABLED_BG_COLOR = new Color(32,32,32);
-    public final static Color TF_DISABLED_FG_COLOR = new Color(70,70,70);
-    public final static Color TF_SELECTED_BORDER_COLOR = new Color(50,80,120);
+    public final static Color TF_DISABLED_FONT_COLOR = new Color(70,70,70);
+    public final static Color TF_SELECTED_BORDER_COLOR = new Color(70,100,140);
     public final static Color RB_MAIN_COLOR = new Color(200,200,200);
     public final static Font MAIN_FONT = new Font("Calibri", Font.PLAIN,12);
-    public final static Font TF_FONT = new Font("Courier New", Font.PLAIN,12);
+    public final static Font TF_FONT = new Font("Courier New", Font.PLAIN,11);
+    public final static Font TF_HINT_FONT = new Font("Courier new", Font.ITALIC, 11);
 }
---
+++ src/com/imagegenerator/gui/MyCheckBox.java
@@ -0,0 +1,60 @@
+package com.imagegenerator.gui;
+
+import com.imagegenerator.Utility;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyCheckBox extends JCheckBox {
+    private int size = 18;
+    private Color currentBG, currentBorder, fontColor, centerColor = Color.WHITE;
+
+    public MyCheckBox(){
+        this.setOpaque(false);
+        this.setMinimumSize(new Dimension(size * 6, size + 10));
+    }
+
+    protected void paintComponent(Graphics g){
+        Graphics2D graph2D = (Graphics2D)g;
+        if (this.isEnabled()) {
+            if (this.isSelected()) {
+                currentBG = Utility.TF_SELECTED_BORDER_COLOR;
+            } else {
+                currentBG = Utility.TF_BG_COLOR;
+                currentBorder = Utility.BORDER_COLOR;
+            }
+        }
+        graph2D.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        graph2D.setColor(currentBG);
+        //graph2D.fillOval(0, 0, size, size);
+        graph2D.setColor(currentBorder);
+        graph2D.drawOval(0, 0, size, size);
+        if(this.isSelected()) {
+            graph2D.setColor(centerColor);
+            graph2D.fillOval(6, 6, size - 11, size - 11);
+        }
+
+        graph2D.setColor(fontColor);
+        graph2D.setFont(Utility.MAIN_FONT);
+        graph2D.drawString(this.getText(), size+10, size-(size/3));
+    }
+
+    public void setEnabled(boolean enabled){
+        super.setEnabled(enabled);
+        if(enabled){
+            currentBG = Utility.TF_BG_COLOR;
+            currentBorder = Utility.BORDER_COLOR;
+            centerColor = Color.WHITE;
+            fontColor = Utility.TF_FONT_COLOR;
+        }
+        else{
+            currentBG = Utility.TF_DISABLED_BG_COLOR;
+            currentBorder = Utility.BORDER_COLOR;
+            centerColor = Color.GRAY;
+            fontColor = Color.GRAY;
+        }
+    }
+}
