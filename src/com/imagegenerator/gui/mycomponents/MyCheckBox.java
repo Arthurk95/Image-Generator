cd510f088a26ea944296d210daa1226930a1cc29
Arthur Kharit <arthurkharit@csus.edu>
2019-06-06 14:33:37 -0700

    Added option to draw text on right or left of circle

--- src/com/imagegenerator/gui/mycomponents/MyCheckBox.java
+++ src/com/imagegenerator/gui/mycomponents/MyCheckBox.java
@@ -8,9 +8,13 @@ import java.awt.*;
 public class MyCheckBox extends JCheckBox {
     private int size;
     private Color currentBG, currentBorder, fontColor, centerColor = Color.WHITE;
+    private int textLocation;
+    public static final int TEXT_LEFT = 0;
+    public static final int TEXT_RIGHT = 1;
 
-    public MyCheckBox(String text, int s){
+    public MyCheckBox(String text, int s, int l){
         size = s;
+        textLocation = l;
         this.setText(text);
         this.setOpaque(false);
         this.setMinimumSize(new Dimension(size * 6, size + 10));
@@ -34,6 +38,51 @@ public class MyCheckBox extends JCheckBox {
         graph2D.setColor(currentBG);
         //graph2D.fillOval(0, 0, size, size);
         graph2D.setColor(currentBorder);
+        if(textLocation == TEXT_RIGHT){
+            textOnRight(graph2D, y);
+        }
+        else textOnLeft(graph2D, y);
+
+
+    }
+
+    public void setEnabled(boolean enabled){
+        super.setEnabled(enabled);
+        if(enabled){
+            currentBG = Utility.TF_BG_COLOR;
+            currentBorder = Utility.TF_BORDER_COLOR;
+            centerColor = Color.WHITE;
+            fontColor = Utility.MAIN_LABEL_COLOR;
+        }
+        else{
+            currentBG = Utility.TF_DISABLED_BG_COLOR;
+            currentBorder = Utility.TF_BORDER_COLOR;
+            centerColor = Color.GRAY;
+            fontColor = Color.GRAY;
+        }
+    }
+
+    /* Draw the circles on the right, text on the left */
+    private void textOnLeft(Graphics2D graph2D, int y){
+        int x = this.getWidth() - size - 2;
+        graph2D.drawOval(x, y, size, size);
+        if(this.isSelected()) {
+            int x2 = x + size/4;
+            int y2 = y + size/4;
+            int radius = size-((size/4)*2) + 1;
+            graph2D.setColor(centerColor);
+            graph2D.fillOval(x2, y2, radius, radius);
+        }
+
+        graph2D.setColor(fontColor);
+        FontMetrics m = graph2D.getFontMetrics(Utility.MAIN_FONT);
+        graph2D.setFont(Utility.MAIN_FONT);
+        graph2D.drawString(this.getText(), x-10-m.stringWidth(this.getText()),
+                y + (size - m.getHeight())/2 + m.getAscent() + 1);
+    }
+
+    /* Draw the circles on the left, text on the right */
+    private void textOnRight(Graphics2D graph2D, int y){
         graph2D.drawOval(0, y, size, size);
         if(this.isSelected()) {
             int x = size/4;
@@ -48,21 +97,5 @@ public class MyCheckBox extends JCheckBox {
         graph2D.setColor(fontColor);
         graph2D.setFont(Utility.MAIN_FONT);
         graph2D.drawString(this.getText(), size+10, y + (size - m.getHeight())/2 + m.getAscent() + 1);
-    }
-
-    public void setEnabled(boolean enabled){
-        super.setEnabled(enabled);
-        if(enabled){
-            currentBG = Utility.TF_BG_COLOR;
-            currentBorder = Utility.TF_BORDER_COLOR;
-            centerColor = Color.WHITE;
-            fontColor = Utility.TF_FONT_COLOR;
-        }
-        else{
-            currentBG = Utility.TF_DISABLED_BG_COLOR;
-            currentBorder = Utility.TF_BORDER_COLOR;
-            centerColor = Color.GRAY;
-            fontColor = Color.GRAY;
-        }
     }
 }
