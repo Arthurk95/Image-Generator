5b556ebacf3b5bd5176cc44f4f9ad99156cea045
Arthur Kharit <arthurkharit@csus.edu>
2019-06-06 12:26:21 -0700

    Checkbox now initialized with starting size and text. Inner circle now always centered within border circle

--- src/com/imagegenerator/gui/mycomponents/MyCheckBox.java
+++ src/com/imagegenerator/gui/mycomponents/MyCheckBox.java
@@ -6,16 +6,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyCheckBox extends JCheckBox {
-    private int size = 18;
+    private int size;
     private Color currentBG, currentBorder, fontColor, centerColor = Color.WHITE;
 
-    public MyCheckBox(){
+    public MyCheckBox(String text, int s){
+        size = s;
+        this.setText(text);
         this.setOpaque(false);
         this.setMinimumSize(new Dimension(size * 6, size + 10));
     }
 
     protected void paintComponent(Graphics g){
         Graphics2D graph2D = (Graphics2D)g;
+        int y = this.getHeight()/2 - size/2;
         if (this.isEnabled()) {
             if (this.isSelected()) {
                 currentBG = Utility.TF_SELECTED_BORDER_COLOR;
@@ -31,15 +34,20 @@ public class MyCheckBox extends JCheckBox {
         graph2D.setColor(currentBG);
         //graph2D.fillOval(0, 0, size, size);
         graph2D.setColor(currentBorder);
-        graph2D.drawOval(0, 0, size, size);
+        graph2D.drawOval(0, y, size, size);
         if(this.isSelected()) {
+            int x = size/4;
+            int y2 = y + size/4;
+            int radius = size-((size/4)*2) + 1;
             graph2D.setColor(centerColor);
-            graph2D.fillOval(6, 6, size - 11, size - 11);
+            graph2D.fillOval(x, y2, radius, radius);
         }
+
+        FontMetrics m = graph2D.getFontMetrics(Utility.MAIN_FONT);
 
         graph2D.setColor(fontColor);
         graph2D.setFont(Utility.MAIN_FONT);
-        graph2D.drawString(this.getText(), size+10, size-(size/3));
+        graph2D.drawString(this.getText(), size+10, y + (size - m.getHeight())/2 + m.getAscent() + 1);
     }
 
     public void setEnabled(boolean enabled){
